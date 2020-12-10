c5fcdc98516b3f0485bde4b168455bffb2c7e1f5
Sky Brewer <jacobrewer@gmail.com>
2020-12-10 14:34:53 +0100

    config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::startup::run;
+
+use zero2prod::configuration::get_configuration;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("localhost:8000").expect("Error binding address");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("localhost:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Error binding address");
     run(listener)?.await
 }
