db3c782988081f68ad1707680f618e3c3c45a917
Sky Brewer <jacobrewer@gmail.com>
2020-12-10 15:56:40 +0100

    update workflow for db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,11 +22,6 @@ async fn health_check_works() {
 #[actix_rt::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
-    let configuration = get_configuration().expect("failed to read config");
-    let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to postgres.");
 
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -41,7 +36,19 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     assert_eq!(200, response.status().as_u16());
 
-    
+    let configuration = get_configuration().expect("failed to read config");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres.");
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_la_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
