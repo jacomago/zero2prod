d1a182da7f76717e206435c45e104e1a0761969c
Sky Brewer <jacobrewer@gmail.com>
2020-12-10 15:50:08 +0100

    connection to db test works

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,15 +1,15 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
-    pub username: String ,
+    pub username: String,
     pub password: String,
     pub port: u16,
-    pub host : String,
-    pub database_name: u16
+    pub host: String,
+    pub database_name: String,
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
@@ -18,4 +18,13 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     settings.merge(config::File::with_name("configuration"))?;
 
     settings.try_into()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,7 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+
+use zero2prod::configuration::get_configuration;
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -19,6 +22,12 @@ async fn health_check_works() {
 #[actix_rt::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("failed to read config");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres.");
+
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -31,6 +40,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    
 }
 
 #[actix_rt::test]
