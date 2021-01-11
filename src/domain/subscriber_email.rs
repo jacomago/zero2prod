921685964e2dc527a4a4659bc03cf221fcb204a1
Sky Brewer <jacobrewer@gmail.com>
2021-01-11 16:16:12 +0100

    test correct email addresss randomly

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -6,7 +6,7 @@ pub struct SubscriberEmail(String);
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
         if validate_email(&s) {
-        Ok(Self(s))
+            Ok(Self(s))
         } else {
             Err(format!("{} is not a valid subscriber email.", s))
         }
@@ -23,7 +23,23 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
