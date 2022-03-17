5440523bd7d2a1fd3a0bedef6423b45064e4d25e
Sky Brewer <jaco.brewer@gmail.com>
2022-03-17 17:55:37 +0100

    starting from the beginning again

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-use zero2prod::configuration::get_configuration;
-
-use zero2prod::startup::Application;
-//Get configuration and call the startup
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2rpod".into(), "info".into());
-    init_subscriber(subscriber);
-
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
-    Ok(())
+fn main() {
 }
