dbd5794ed976718f9aefc96aeeb014f84a12980d
Sky Brewer <jaco.brewer@gmail.com>
2022-03-17 17:56:12 +0100

    add very basic version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::net::TcpListener;
+use zero2prod::run;
 
-fn main() {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
