1e251bdf1aee2ab5260680e57e8242a9b7b8e230
Sky Brewer <jaco.brewer@gmail.com>
2022-03-21 11:07:40 +0100

    add config file

--- src/configuration.rs
+++ src/configuration.rs
@@ -19,7 +19,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Add configuration values from a file named `configuration`.
     // It will look for any top-level file with an extension
     // that `config` knows how to parse: yaml, json, etc.
-    settings.merge(config::File::with_name("configuration"))?;
+    settings.merge(config::File::with_name("local_configuration"))?;
     // Try to convert the configuration values it read into
     // our Settings type
     settings.try_into()
