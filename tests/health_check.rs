6d4cd6cbb0edf03aaf567520dd8113389c7b88af
Sky Brewer <jaco.brewer@gmail.com>
2022-03-21 11:18:40 +0100

    add test fix typo

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use sqlx::{PgConnection, Connection};
+use zero2prod::{startup::run, configuration::get_configuration};
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -34,6 +35,11 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+
+    let configuration = get_configuration().expect("Failed to read config");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string).await.expect("Failed to connect to postgres");
+
     let client = reqwest::Client::new();
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
