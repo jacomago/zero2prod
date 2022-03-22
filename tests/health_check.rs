2cbe6c856032e123cad8d001233be00c4c959307
Sky Brewer <jaco.brewer@gmail.com>
2022-03-22 15:14:26 +0100

    fix repeatable tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,31 @@
-use sqlx::PgPool;
+use sqlx::{PgPool, PgConnection, Connection, Executor};
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
+use uuid::Uuid;
+use zero2prod::{configuration::{get_configuration, DatabaseSettings}, startup::run};
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+    connection_pool
 }
 
 async fn spawn_app() -> TestApp {
@@ -15,10 +36,10 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://{}:{}", local_address, port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let db_pool = configure_database(&configuration.database).await;
 
     let server = run(listener, db_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
