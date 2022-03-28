d6ca5f8767d72550ff56468dcd22a1f53ddd1eef
Sky Brewer <jaco.brewer@gmail.com>
2022-03-28 13:56:50 +0200

    log level add

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,29 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::{
     configuration::{get_configuration, DatabaseSettings},
     startup::run,
 };
+
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    // We cannot assign the output of `get_subscriber` to a variable based on the value
+    // of `TEST_LOG` because the sink is part of the type returned by `get_subscriber`,
+    // therefore they are not the same type. We could work around it, but this is the
+    // most straight-forward way of moving forward.
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -32,6 +51,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
     let local_address = "127.0.0.1";
     let listener = TcpListener::bind(format!("{}:{}", local_address, "0"))
         .expect("Failed to bind random port");
