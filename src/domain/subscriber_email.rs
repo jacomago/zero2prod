12c054b644b9814f319c9da9a327e4c7009fd3d1
Sky Brewer <jaco.brewer@gmail.com>
2022-03-30 10:32:06 +0200

    fix use of quickcheck

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -26,20 +26,6 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
 
-    #[derive(Debug, Clone)]
-    struct ValidEmailFixture(pub String);
-    
-    impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
-            Self(email)
-        }
-    }
-
-    #[quickcheck_macros::quickcheck]
-    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
-        SubscriberEmail::parse(valid_email.0).is_ok()
-    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -55,5 +41,21 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
