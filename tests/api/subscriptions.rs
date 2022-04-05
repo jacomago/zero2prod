aecbd35eb038116229bb4bc9f18d12b2803a7812
Sky Brewer <jaco.brewer@gmail.com>
2022-04-05 15:46:13 +0200

    Add a response check

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -74,16 +74,16 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
-        .expect(1)
         .mount(&app.email_server)
         .await;
 
     // Act
-    app.post_subscriptions(body.into()).await;
+    let response = app.post_subscriptions(body.into()).await;
 
     // Assert
-    // Mock asserts on drop
+    assert_eq!(200, response.status().as_u16());
 }
