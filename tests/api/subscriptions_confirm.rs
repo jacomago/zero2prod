0571f5aa734ddbc53a14638afe7b26d7f618dbc1
Sky Brewer <jaco.brewer@gmail.com>
2022-04-19 17:55:20 +0200

    test invalid token input

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -14,6 +14,20 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
     assert_eq!(response.status().as_u16(), 400);
 }
 
+#[tokio::test]
+async fn confirmations_without_valid_token_are_rejected_with_a_400() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act
+    let response = reqwest::get(&format!("{}/subscriptions/confirm?subscription_token=f", app.address))
+        .await
+        .unwrap();
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 400);
+}
+
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
