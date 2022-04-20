26878d4a47732efc59cf380b52cb74a6a25ba3fc
Sky Brewer <jaco.brewer@gmail.com>
2022-04-20 17:28:01 +0200

    add publish error and get confrimed subscribers

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,18 +10,8 @@ use sqlx::{PgPool, Postgres, Transaction};
 use tera::Tera;
 use uuid::Uuid;
 
-fn error_chain_fmt(
-    e: &impl std::error::Error,
-    f: &mut std::fmt::Formatter<'_>,
-) -> std::fmt::Result {
-    writeln!(f, "{}\n", e)?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{}", cause)?;
-        current = cause.source();
-    }
-    Ok(())
-}
+use super::error_chain_fmt;
+
 
 #[derive(thiserror::Error)]
 pub enum SubscribeError {
