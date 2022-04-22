365ae49676f36cc8e560fa643490110ca0a8c001
Sky Brewer <jaco.brewer@gmail.com>
2022-04-22 17:16:25 +0200

    use reqwest cookie feature

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,6 +1,3 @@
-use std::collections::HashSet;
-
-use reqwest::header::HeaderValue;
 
 use crate::helpers::assert_is_redirect_to;
 use crate::helpers::spawn_app;
@@ -19,10 +16,6 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     // Assert
     assert_is_redirect_to(&response, "/login");
-    let cookies: HashSet<_> = response
-        .headers()
-        .get_all("Set-Cookie")
-        .into_iter()
-        .collect();
-    assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
