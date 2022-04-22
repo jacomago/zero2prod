95e45a2bd20f062fd46701fd62ad38d84f71afd0
Sky Brewer <jaco.brewer@gmail.com>
2022-04-22 14:08:12 +0200

    check the hmac tag

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,22 +1,50 @@
 use actix_web::{http::header::ContentType, web, HttpResponse};
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
 use tera::Context;
 
-use crate::routes::TEMPLATES;
+use crate::{routes::TEMPLATES, startup::HmacSecret};
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
-    error: Option<String>,
+    error: String,
+    tag: String,
 }
 
-pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
+}
+
+pub async fn login_form(
+    query: Option<web::Query<QueryParams>>,
+    secret: web::Data<HmacSecret>,
+) -> HttpResponse {
     let mut login_context = Context::new();
-    login_context.insert(
-        "error_message",
-        &match query.0.error {
-            None => "".into(),
-            Some(message) => message,
+    let error_message = match query {
+        None => "".into(),
+        Some(query) => match query.0.verify(&secret) {
+            Ok(error) => error,
+            Err(e) => {
+                tracing::warn!(
+                error.message = %e,
+                error.cause_chain = ?e,
+                "Failed to verify query parameters using the HMAC tag"
+                );
+                "".into()
+            }
         },
-    );
+    };
+    login_context.insert("error_message", &error_message);
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(TEMPLATES.render("login.html", &login_context).unwrap())
