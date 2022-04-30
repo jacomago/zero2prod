904e069f1f1a2c9817e1aab3585c3ae30ea46b0c
Sky Brewer <jaco.brewer@gmail.com>
2022-04-30 17:14:20 +0200

    redirect bad logins to dashboard

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -55,6 +55,18 @@ impl TestApp {
             .unwrap()
     }
 
+    pub async fn get_admin_dashboard_html(&self) -> reqwest::Response {
+        self.api_client
+            .get(&format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request")
+    }
+
+    pub async fn get_admin_dashboard(&self) -> String {
+        self.get_admin_dashboard_html().await.text().await.unwrap()
+    }
+
     pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
     where
         Body: serde::Serialize,
@@ -66,17 +78,6 @@ impl TestApp {
             .send()
             .await
             .expect("Failed to execute request.")
-    }
-
-    pub async fn get_admin_dashboard(&self) -> String {
-        self.api_client
-            .get(&format!("{}/admin/dashboard", &self.address))
-            .send()
-            .await
-            .expect("Failed to execute request.")
-            .text()
-            .await
-            .unwrap()
     }
 
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
