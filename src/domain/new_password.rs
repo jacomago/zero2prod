fd3285873136f086c16e6a79f3a752ea6307116b
Sky Brewer <jaco.brewer@gmail.com>
2022-05-16 12:36:13 +0200

    quick check password test

--- src/domain/new_password.rs
+++ src/domain/new_password.rs
@@ -31,24 +31,12 @@ impl NewPassword {
     }
 }
 
-// # 	Description 	L1 	L2 	L3 	CWE 	NIST §
-// 2.1.1 	Verify that user set passwords are at least 12 characters in length (after multiple spaces are combined). (C6) 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.2 	Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied. (C6) 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.3 	Verify that password truncation is not performed. However, consecutive multiple spaces may be replaced by a single space. (C6) 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.4 	Verify that any printable Unicode character, including language neutral characters such as spaces and Emojis are permitted in passwords. 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.5 	Verify users can change their password. 	✓ 	✓ 	✓ 	620 	5.1.1.2
-// 2.1.6 	Verify that password change functionality requires the user's current and new password. 	✓ 	✓ 	✓ 	620 	5.1.1.2
-// 2.1.7 	Verify that passwords submitted during account registration, login, and password change are checked against a set of breached passwords either locally (such as the top 1,000 or 10,000 most common passwords which match the system's password policy) or using an external API. If using an API a zero knowledge proof or other mechanism should be used to ensure that the plain text password is not sent or used in verifying the breach status of the password. If the password is breached, the application must require the user to set a new non-breached password. (C6) 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.8 	Verify that a password strength meter is provided to help users set a stronger password. 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.9 	Verify that there are no password composition rules limiting the type of characters permitted. There should be no requirement for upper or lower case or numbers or special characters. (C6) 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.10 	Verify that there are no periodic credential rotation or password history requirements. 	✓ 	✓ 	✓ 	263 	5.1.1.2
-// 2.1.11 	Verify that "paste" functionality, browser password helpers, and external password managers are permitted. 	✓ 	✓ 	✓ 	521 	5.1.1.2
-// 2.1.12 	Verify that the user can choose to either temporarily view the entire masked password, or temporarily view the last typed character of the password on platforms that do not have this as built-in functionality.
 #[cfg(test)]
 mod tests {
     use crate::domain::NewPassword;
     use claim::{assert_err, assert_ok};
     use fake::faker::internet::en::Password;
+    use fake::Fake;
     use secrecy::Secret;
 
     #[test]
@@ -72,5 +60,20 @@ mod tests {
     fn a_pass_longer_than_128_graphemes_is_rejected() {
         let pass = Secret::new("a".repeat(129));
         assert_err!(NewPassword::parse(pass));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidPasswordFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidPasswordFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let password = Password(12..128).fake_with_rng(g);
+            Self(password)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_password_are_parsed_successfully(valid_password: ValidPasswordFixture) -> bool {
+        NewPassword::parse(Secret::new(valid_password.0)).is_ok()
     }
 }
