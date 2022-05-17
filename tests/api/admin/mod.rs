fceac96bffbe09321e95f0377918cd7c2f9ff538
Sky Brewer <jaco.brewer@gmail.com>
2022-05-17 17:56:36 +0200

    remove basic auth from test path

--- tests/api/admin/mod.rs
+++ tests/api/admin/mod.rs
@@ -52,7 +52,6 @@ impl TestApp {
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
         self.api_client
             .post(&format!("{}/admin/newsletters", &self.address))
-            .basic_auth(&self.test_user.username, Some(&self.test_user.password))
             .json(&body)
             .send()
             .await
