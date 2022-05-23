bf0a635c0a4a5f0b7d165f30d0c1e4e8cc1a1a67
Sky Brewer <jaco.brewer@gmail.com>
2022-05-23 12:04:45 +0200

    adding tracing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use zero2prod::issue_delivery_worker::run_worker_until_stopped;
 use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
+use std::fmt::{Debug, Display};
+use tokio::task::JoinError;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
@@ -14,8 +17,33 @@ async fn main() -> anyhow::Result<()> {
     let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
 
     tokio::select! {
-        _ = application_task => {},
-        _ = worker_task => {},
+        o = application_task => report_exit("API", o),
+        o = worker_task => report_exit("Background worker", o),
     };
+
     Ok(())
+}
+
+fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{} failed",
+                task_name
+            )
+        }
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{}' task failed to complete",
+                task_name
+            )
+        }
+    }
 }
